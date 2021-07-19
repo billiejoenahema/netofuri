69829b3dc85ecd55c6545bd0e0925716a45ffcf1
billiejoenahema <billiejoenahema@gmail.com>
2021-07-19 19:53:15 +0900

    implement: fetch data & show items

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import instance from '../axios'
+import './Row.scss'
 
 export const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([])
+  const base_url = 'https://image.tmdb.org/t/p/original'
 
   useEffect(() => {
     async function fetchData() {
@@ -13,10 +15,20 @@ export const Row = ({ title, fetchUrl }) => {
     fetchData()
   }, [fetchUrl])
 
-  console.log(movies)
-
   return (
-    <div className="Row" />
+    <div className="Row">
+      <h2>{title}</h2>
+      <div className="Row-posters">
+        {movies.map((movie) => (
+          < img
+            key={movie.id}
+            className="Row-poster"
+            src={`${base_url}${movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
