da719db68378328c5c18341ca4c77785d7275828
billiejoenahema <billiejoenahema@gmail.com>
2021-07-21 20:01:15 +0900

    apply: enlarge first Row image

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from 'react'
 import instance from '../axios'
 import './Row.scss'
 
-export const Row = ({ title, fetchUrl }) => {
+export const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([])
   const baseUrl = 'https://image.tmdb.org/t/p/original'
   const noImage = `${process.env.PUBLIC_URL}/no_image.jpeg`
+  const getMovieImage = (movie) => {
+    if (movie.poster_path && movie.backdrop_path) {
+      return (`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`)
+    }
+    return noImage
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -23,13 +29,13 @@ export const Row = ({ title, fetchUrl }) => {
         {movies.map((movie) => (
           < img
             key={movie.id}
-            className="Row-poster"
-            src={movie.backdrop_path ? `${baseUrl}${movie.backdrop_path}` : noImage}
+            className={`Row-poster ${isLargeRow && 'Row-poster-large'}`}
+            src={getMovieImage(movie)}
             alt={movie.name}
           />
         ))}
       </div>
-    </div>
+    </div >
   )
 }
 
