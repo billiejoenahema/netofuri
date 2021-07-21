580aa2567a33b750946da19b2dabfe842e350ef3
billiejoenahema <billiejoenahema@gmail.com>
2021-07-22 08:14:26 +0900

    create: Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { requests } from './request'
+import { truncateDescription } from '../truncateDescription'
+
+const Banner = () => {
+  const [movie, setMovie] = useState({})
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals)
+      console.log(request.data.result)
+
+      //apiからランダムで値を取得している
+      setMovie(
+        request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request
+    }
+    fetchData()
+  }, [])
+  console.log(movie)
+
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="Banner-contents">
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncateDescription(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom" />
+    </header>
+  )
+}
+export default Banner
