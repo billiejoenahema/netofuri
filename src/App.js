9f2200e43d37e91deae818f133a2e69ee2e65ee3
billiejoenahema <billiejoenahema@gmail.com>
2021-07-22 09:32:50 +0900

    add: Banner to App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react'
 import './App.css'
+import Banner from './components/Banner'
 import Row from './components/Row'
 import { requests } from './request'
 
 const App = () => {
   return (
-    <div className="App">
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
-    </div>
+    <>
+      <Banner />
+      <div className="App">
+        <Row
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        {/* <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> */}
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        {/* <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} /> */}
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
+      </div>
+    </>
   )
 }
 
